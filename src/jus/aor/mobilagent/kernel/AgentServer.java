dc252a9433b0b61f59046c4527346715daf92208
ProfesseurSid <salutlesmanchots@hotmail.fr>
2018-03-16 12:09:45 +0100

    Creation de l'arborescence complète

---
+++ src/jus/aor/mobilagent/kernel/AgentServer.java
@@ -0,0 +1,11 @@
+package jus.aor.mobilagent.kernel;
+
+public class AgentServer implements _Service<Object> {
+
+	@Override
+	public Object call(Object... params) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/jus/aor/mobilagent/kernel/BAMServerClassLoader.java
@@ -0,0 +1,13 @@
+package jus.aor.mobilagent.kernel;
+
+import java.net.URL;
+import java.net.URLClassLoader;
+
+public class BAMServerClassLoader extends URLClassLoader {
+
+	public BAMServerClassLoader(URL[] arg0, ClassLoader classLoader) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+}
