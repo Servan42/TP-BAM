d3eea3b59a260bcabfcea674f61631d0c5857333
ProfesseurSid <salutlesmanchots@hotmail.fr>
2018-03-29 16:08:28 +0200

    Creation des 4 serveurs d'hotels

--- source/Server.java
+++ source/Server.java
@@ -12,10 +12,6 @@ import org.xml.sax.SAXException;
 
 public class Server {
 	public static void main(String args[]) {
-		
-		
-		
-
 		/* récupération d'informations de configuration */
 		DocumentBuilder docBuilder;
 		try {
@@ -25,20 +21,35 @@ public class Server {
 			//On récupère les arguments pour la construction de Chaine
 			String arguments = doc.getElementsByTagName("service").item(0).getAttributes().getNamedItem("args").getNodeValue();
 			Chaine server1 = new Chaine(arguments);
-			LocateRegistry.createRegistry(2001);
-			java.rmi.Naming.bind("//localhost:2001/Chaine1", server1);
+			LocateRegistry.createRegistry(1111);
+			java.rmi.Naming.bind("//localhost:1111/Chaine1", server1);
 			
-			//TODO répéter de doc=... à ici en changeant arg[x]
-			// les arguments seront hello.serverX.xml
+			doc = docBuilder.parse(new File(args[1]));
+			//On récupère les arguments pour la construction de Chaine
+			arguments = doc.getElementsByTagName("service").item(0).getAttributes().getNamedItem("args").getNodeValue();
+			Chaine server2 = new Chaine(arguments);
+			LocateRegistry.createRegistry(2222);
+			java.rmi.Naming.bind("//localhost:2222/Chaine2", server2);
 			
+			doc = docBuilder.parse(new File(args[2]));
+			//On récupère les arguments pour la construction de Chaine
+			arguments = doc.getElementsByTagName("service").item(0).getAttributes().getNamedItem("args").getNodeValue();
+			Chaine server3 = new Chaine(arguments);
+			LocateRegistry.createRegistry(3333);
+			java.rmi.Naming.bind("//localhost:3333/Chaine3", server3);
+			
+			doc = docBuilder.parse(new File(args[3]));
+			//On récupère les arguments pour la construction de Chaine
+			arguments = doc.getElementsByTagName("service").item(0).getAttributes().getNamedItem("args").getNodeValue();
+			Chaine server4 = new Chaine(arguments);
+			LocateRegistry.createRegistry(4444);
+			java.rmi.Naming.bind("//localhost:4444/Chaine4", server4);
+
 		} catch (SAXException | IOException e1) {
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		} catch (ParserConfigurationException e1) {
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		} catch (AlreadyBoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
@@ -49,23 +60,5 @@ public class Server {
 //		if(System.getSecurityManager() == null){
 //			System.setSecurityManager(new SecurityManager());
 //		}
-		try {
-//			Chaine server1 = new Chaine(new Hotel("Sofitel", "Paris"), new Hotel("Sofitel", "Lyon"));
-//			Chaine server2 = new Chaine(new Hotel("Ibis1", "Grenoble"), new Hotel("Ibis", "Bordeaux"), new Hotel("Ibis2", "Grenoble"));
-//			Chaine server3 = new Chaine(new Hotel("B&B", "Paris"), new Hotel("B&B1", "Lyon"), new Hotel("B&B2", "Lyon"));
-//			Chaine server4 = new Chaine();
-			
-			LocateRegistry.createRegistry(1111);
-			LocateRegistry.createRegistry(2018);
-			LocateRegistry.createRegistry(3008);
-			
-//			java.rmi.Naming.bind("//localhost:1111/Chaine2", server2);
-//			java.rmi.Naming.bind("//localhost:2018/Chaine3", server3);
-//			java.rmi.Naming.bind("//localhost:3008/Chaine4", server4);
-			
-			System.out.println("Serveur et registre operationels");
-		} catch(Exception e) {
-			System.out.println("C'est la panique "+e.toString());
-		}
 	}
 }
