927234455e47d0e5197d708a5de9c32bf9ce4684
Henry Saivet <death.note38@hotmail.fr>
2018-04-04 17:18:47 +0200

    k

--- src/jus/aor/mobilagent/hello/Hello.java
+++ src/jus/aor/mobilagent/hello/Hello.java
@@ -1,38 +1,38 @@
 package jus.aor.mobilagent.hello;
 
-import java.net.URI;
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.LinkedList;
-import java.util.List;
-
 import jus.aor.mobilagent.kernel._Action;
-import jus.aor.mobilagent.kernel.Agent;
 
 /**
  * Classe de test élémentaire pour le bus à agents mobiles
- * @author  Morat
+ * 
+ * @author Morat
  */
-public class Hello extends Agent{
+public class Hello extends Agent {
 
-	 /**
-	  * construction d'un agent de type hello.
-	  * @param args aucun argument n'est requis
-	  */
-	 public Hello(Object... args) {
-		 // ....
-	 }
-	 /**
-	 * l'action à entreprendre sur les serveurs visités  
+	/**
+	 * construction d'un agent de type hello.
+	 * 
+	 * @param args
+	 *            aucun argument n'est requis
 	 */
-	protected _Action doIt = new _Action(){
+	public Hello(Object... args) {
+		// ....
+	}
+
+	/**
+	 * l'action à entreprendre sur les serveurs visités
+	 */
+	protected _Action doIt = new _Action() {
 		// ...
 	};
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see jus.aor.mobilagent.kernel.Agent#retour()
 	 */
 	@Override
-	protected _Action retour(){
+	protected _Action retour() {
 		// return ...;
 	}
 	// ...
