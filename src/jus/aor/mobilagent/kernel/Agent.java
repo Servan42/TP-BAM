bf296c207cd0c5e084529bbd6848ccf4982431c7
Servan42 <charlot.servan@gmail.com>
2018-04-04 18:45:26 +0200

    Suite infrastructure

--- src/jus/aor/mobilagent/kernel/Agent.java
+++ src/jus/aor/mobilagent/kernel/Agent.java
@@ -1,11 +1,17 @@
 package jus.aor.mobilagent.kernel;
 
+import java.net.URL;
 import java.util.List;
 
 import jus.aor.mobilagent.kernel.AgentServer;
 import jus.aor.mobilagent.kernel.Etape;
 
 public class Agent implements _Agent {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	@Override
 	public void run() {
@@ -39,7 +45,35 @@ public class Agent implements _Agent {
 	public void init(List<ServiceDescriptor> list) {
 		// TODO Auto-generated method stub
 		System.out.println("NOT IMPLEMETED YET");
-		
+
+	}
+
+	protected _Action retour() {
+		System.out.println("NOT IMPLEMETED YET");
+		return null;
+	}
+
+	protected _Service getService(String string) {
+		System.out.println("NOT IMPLEMETED YET");
+		return null;
+	}
+
+	private void move() {
+		System.out.println("NOT IMPLEMETED YET");
+	}
+
+	protected void move(URL url) {
+		System.out.println("NOT IMPLEMETED YET");
+	}
+
+	protected String route() {
+		System.out.println("NOT IMPLEMETED YET");
+		return null;
+	}
+
+	public String toString() {
+		System.out.println("NOT IMPLEMETED YET");
+		return null;
 	}
 
 }
