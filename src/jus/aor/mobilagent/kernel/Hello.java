8b26c4b7cb9b86a1b9fedfa2ef1855a4af6ea258
ProfesseurSid <salutlesmanchots@hotmail.fr>
2018-04-05 16:34:29 +0200

    implémentation de retour()

--- src/jus/aor/mobilagent/kernel/Hello.java
+++ src/jus/aor/mobilagent/kernel/Hello.java
@@ -26,28 +26,12 @@ public class Hello extends Agent {
 	/**
 	 * construction d'un agent de type hello.
 	 * 
-	 * // * @param args // * Route initialisee mais potentiellement incomplete,
-	 * List<String> // * d'addresse de serveurs, List<String> d'actions associees
-	 * 
 	 * @param args
 	 *            le serveur hébergeant initialement l'agent, le nom logique du
 	 *            serveur d'agents
 	 */
 	public Hello(Object... args) {
-		// this.route = (Route) args[0];
-		// if (args[1] != null && args[2] != null)
-		// for (int i = 0; i < ((List<String>) args[1]).size(); i++) {
-		// try {
-		// route.add(new Etape(new URI(((List<String>) args[1]).get(i)),
-		// (_Action) this.getClass().getField(((List<String>)
-		// args[2]).get(i)).get(this)));
-		// } catch (IllegalArgumentException | IllegalAccessException |
-		// NoSuchFieldException | SecurityException
-		// | URISyntaxException e) {
-		// e.printStackTrace();
-		// }
-		// }
-		init((AgentServer)args[0], (String)args[1]);
+		init((AgentServer) args[0], (String) args[1]);
 	}
 
 	/**
@@ -69,12 +53,13 @@ public class Hello extends Agent {
 	 */
 	@Override
 	protected _Action retour() {
-		// return ... TODO;
-		System.out.println(toString() + "retour() NOT IMPLEMENTED YET");
-		return null;
+		return new _Action() { 
+			private static final long serialVersionUID = -3742771451224038951L;
+			
+			public void execute() {System.out.println("J'AI FINI !");}
+		};
 	}
-	// ...
-
+	
 	public String toString() {
 		return "Classe Hello.java";
 	}
