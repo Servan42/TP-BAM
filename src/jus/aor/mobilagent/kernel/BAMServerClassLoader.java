6c05f4ee13af7789f076fcf7b6d872f0a5282e9b
ProfesseurSid <salutlesmanchots@hotmail.fr>
2018-04-05 18:28:54 +0200

    Tentative d'implémentation des ClassLoader

--- src/jus/aor/mobilagent/kernel/BAMServerClassLoader.java
+++ src/jus/aor/mobilagent/kernel/BAMServerClassLoader.java
@@ -4,20 +4,19 @@ import java.net.URL;
 import java.net.URLClassLoader;
 
 public class BAMServerClassLoader extends URLClassLoader {
-
+	
+	
 	public BAMServerClassLoader(URL[] arg0, ClassLoader classLoader) {
-		super(arg0);
+		super(arg0, classLoader);
 		// TODO Auto-generated constructor stub BAM
 	}
 
 	protected void addURL(URL url) {
-		// TODO
-		System.out.println(toString() + " addURL(URL) NOT IMPLEMETED YET");
+		super.addURL(url);
 	}
 
 	public String toString() {
-		// TODO
-		return "Classe BAMServerClassLoader";
+		return "BAMServerClassLoader " + super.toString();
 	}
 
 }
