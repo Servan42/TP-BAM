f9fef3752dbb8cac85507b9f76b49e2263f8343c
Servan42 <charlot.servan@gmail.com>
2018-04-11 00:08:03 +0200

    Pretty much done

--- src/jus/aor/mobilagent/kernel/Starter.java
+++ src/jus/aor/mobilagent/kernel/Starter.java
@@ -24,6 +24,9 @@ import org.w3c.dom.NodeList;
 
 /**
  * @author Morat
+ * Arguments for ServerX : Configurations/hostel.serverX.xml ServerX
+ * Arguments for Client1 : Configurations/hostel.client1.xml Client1
+ * Argument JVM : -Djava.security.policy="/home/drzed/Documents/Polytech/S8/AR/TP-BAM/BAM-policy" -Dsun.lang.ClassLoader.allowArraySyntax=true
  */
 public class Starter {
 	/** le document xml en cours */
